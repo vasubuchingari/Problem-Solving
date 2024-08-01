f543c0c6349c67ec0b0375d791488210f02e373f
Vasu Buchingari <vasubuchingari@gmail.com>
2024-08-01 16:19:23 +0530

    Update 1_two_sum.py

--- Leetcode/array/1_two_sum.py
+++ Leetcode/array/1_two_sum.py
@@ -7,3 +7,17 @@ class Solution:
                     l.append(i)
                     l.append(j)
                     return l
+
+
+# 2nd solution
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         di={}
+#         for i,x in enumerate(nums):
+#             n2=target-x
+#             if n2 in di:
+#                 return (di[n2],i)
+#             elif x not in di:
+#                 di[x]=i
+            
+

126d77a39cd4f6e6c316356aae2a73f98df00455
Vasu Buchingari <vasubuchingari@gmail.com>
2024-08-05 22:16:43 +0530

    Update 3238_find_the_number_of_winning_players.py

--- Leetcode/array/3238_find_the_number_of_winning_players.py
+++ Leetcode/array/3238_find_the_number_of_winning_players.py
@@ -1,1 +1,25 @@
-
+class Solution:
+    def winningPlayerCount(self, n: int, pick: List[List[int]]) -> int:
+        match={}
+        for x in pick:
+            if x[0] in match:
+                match[x[0]].append(x[1])
+            else:
+                match[x[0]]=list()
+                match[x[0]].append(x[1])
+        res=0
+        winning_keys = []
+        for key,value in match.items():
+            count_dic={}
+            for x in value:
+                if x in count_dic:
+                    count_dic[x]+=1
+                else:
+                    count_dic[x]=1
+            if any(count >= key + 1 for count in count_dic.values()):
+                winning_keys.append(key)
+        return len(winning_keys)
+                
+        print(match)
+        return res
+        

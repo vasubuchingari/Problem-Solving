9fc2717ab3aa05c4a7fd4ce58bcb91f0f2a4a7da
Vasu Buchingari <vasubuchingari@gmail.com>
2024-08-07 09:33:41 +0530

    Create 82__remove_duplicates_from_sorted_list_II_medium.py

---
+++ Leetcode/Linked list/Singly Linked list/82__remove_duplicates_from_sorted_list_II_medium.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy=ListNode(next=head)
+        prev=dummy
+        curr=head
+        while curr:
+            if curr.next and curr.val==curr.next.val:
+                while curr.next and curr.val==curr.next.val:
+                    curr=curr.next
+                prev.next=curr.next
+            else:
+                prev=prev.next
+            curr=curr.next
+        return dummy.next
+        
